3e5ff2337fae49aad5bdb52623b69920fd1fe87e
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2022-05-22 08:54:06 +0530

    .

--- src/Discord/slashCommands/help.js
+++ src/Discord/slashCommands/help.js
@@ -55,7 +55,7 @@ function getSingleCommandEmbed(command, client) {
 
 function getCommandsEmbed(client) {
   let description = ``;
-  let commands = new Map([...client.slashCommands.map(c => c.name = "/" + c), ...client.commands.map(c => c.name = client.config.prefix + c)]);
+  let commands = new Map([...client.slashCommands.forEach(c => c.set("name", "/" + c)), ...client.commands.forEach(c => c.set("name", client.config.prefix + c))]);
   commands.forEach(function (command) {
     description += `**${command.name}**: ${command?.description}\n`;
   });
